8afab5727e41c32ebcda54b5251a429013de0d86
chep267 <66619015+chep267@users.noreply.github.com>
2020-06-15 15:48:50 +0700

    Update Ktra.java

--- Ktra.java
+++ Ktra.java
@@ -5,27 +5,10 @@ import java.io.File;
 
 public class Ktra {
 
-    private void fetchChild(File file) {
-
-        System.out.println(file.getAbsolutePath());
-
-        if (file.isDirectory()) {
-
-            File[] children = file.listFiles();
-
-            for (File child : children) {
-                // Đệ quy (Recursive)
-                this.fetchChild(child);
-            }
-        }
-
-    }
-
     public static void main(String[] args) {
-        Ktra example = new Ktra();
-        File f = new File("/home/dongnt");
-        example.fetchChild(f);
-
+        ThuMuc example = new ThuMuc();
+        File f = new File("/home/dongnt/Downloads");
+        example.mo(f);
     }
 
 }
