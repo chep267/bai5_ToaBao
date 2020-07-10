6bc0e561209ea417c8f965a6ce1b1830237516f3
Nguyen Thanh Dong <66619015+chep267@users.noreply.github.com>
2020-07-10 09:51:15 +0700

    Update NhaBao.java

--- NhaBao.java
+++ NhaBao.java
@@ -15,9 +15,13 @@ class NhaBao extends NhanVien{
         System.out.print("\n\tLoai nhan vien: Nha bao");
         System.out.print("\n\tSo bai dang: "+sobd);
     }
-
+    public void inThongTin() {
+        super.inThe();
+        System.out.print("\n\tSo bai dang: "+sobd);
+    }
 
     // get, set:
-    public int get_SoBaiDang() {return sobd;}
-    public void set_SoBaiDang( int so) {sobd = so;}
+    public int getSoBaiDang() {return sobd;}
+    public void setSoBaiDang( int so) {sobd = so;}
+
 }
